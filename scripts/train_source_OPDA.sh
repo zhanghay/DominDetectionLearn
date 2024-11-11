471a1bf9b432db48dec4f83f42df0d3a7306c8a2
zhanghay <2867334718@qq.com>
2024-11-11 21:21:42 +0800

    第一次运行

--- scripts/train_source_OPDA.sh
+++ scripts/train_source_OPDA.sh
@@ -15,7 +15,7 @@ python train_source.py  --dataset OfficeHome --s_idx 2  --target_label_type OPDA
 python train_source.py  --dataset OfficeHome --s_idx 3  --target_label_type OPDA --epochs 50 --lr 0.01 
 
 echo "OPDA SOURCE TRAIN ON VisDA"
-python train_source.py --backbone_arch resnet50 --dataset VisDA --s_idx 0  --target_label_type OPDA --epochs 10 --lr 0.001
+python train_source.py --backbone_arch resnet50 --dataset VisDA1 --s_idx 0  --target_label_type OPDA --epochs 10 --lr 0.001
 
 echo "OPDA SOURCE TRAIN ON DomainNet"
 python train_source.py  --dataset DomainNet --s_idx 0  --target_label_type OPDA --epochs 50 --lr 0.01 
